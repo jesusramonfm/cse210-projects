843ee582697cd99f3edc53516052e9be8da811b4
ramonj10 <104482455+ramonj10@users.noreply.github.com>
2023-11-05 08:08:15 +0100

    "Week 1 exercise"

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        string letter;
+        Console.WriteLine("Type in your grade percentage: ");
+        int gradePercentage = int.Parse(Console.ReadLine());
+        if (gradePercentage >= 90)
+        {
+            letter = "A";
+        }
+        else if (gradePercentage >= 80)
+        {
+            letter = "B";        
+        }      
+        else if (gradePercentage >= 70)
+        {
+            letter = "C";
+        }
+        else if (gradePercentage >= 60)
+        {
+            letter = "D";
+        }
+        else 
+        {
+            letter = "F";
+        }
+        Console.WriteLine($"Your grade is {letter}");
+        
+        if (gradePercentage >= 70)
+        {
+            Console.WriteLine("Well done, you pass the course");
+        }
+        else
+        {
+            Console.WriteLine("You didn´t pass the course this time. Try again");
+        }
     }
 }
