6a5c9f2cfb89b1a425f34a739712a1417086b118
ramonj10 <104482455+ramonj10@users.noreply.github.com>
2023-11-07 08:43:09 +0100

    " Week 01 assignment"

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,34 @@
 using System;
-
+using System.Collections.Generic;
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        int newNumber;
+        int average = 0;
+        int largest = 0;
+        Console.WriteLine("Enter a list of numbers, type 0 when finished. ");
+        do
+        {
+            newNumber = int.Parse(Console.ReadLine());
+            numbers.Add(newNumber);
+        } while (newNumber != 0);
+
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+            if (largest < number)
+            {
+                largest  =  number;
+            }
+        }
+        
+        average = sum / (numbers.Count - 1);   // -1  t remove the last 0s 
+
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest number is: {largest}");
     }
 }
