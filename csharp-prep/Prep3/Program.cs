74ad819c6b3132bee629403eccaefca490200f92
ramonj10 <104482455+ramonj10@users.noreply.github.com>
2023-11-07 07:36:19 +0100

    " Week  01 assignment "

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        //Console.WriteLine("What is the magic number? ");
+        //int magicNumber = int.Parse(Console.ReadLine());
+        Random  randomNumber = new Random();
+        int magicNumber = randomNumber.Next(1,100);
+        int yourNumber;
+        do
+        {
+            Console.WriteLine("What is your guess? ");
+            yourNumber = int.Parse(Console.ReadLine());
+
+            if (magicNumber < yourNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (magicNumber > yourNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else
+            {
+                Console.WriteLine("You  guessed it!");
+            }
+        } while (magicNumber != yourNumber);
     }
 }
